2325b56abc2d1a714a59f7463cdd8c5a4fab6199
alirezaSoleymani <a83.soleimani@gmail.com>
2026-02-23 18:24:20 +0330

    feat: create Ticket class, connect to Column class, and make some general modfications

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,16 @@ import './style.scss';
 class Board {
    private board: HTMLDivElement;
    private columns: Column[] = [
-      new Column(0, 'To-Do', Status.ToDo),
-      new Column(1, 'In Progress', Status.InProgress),
-      new Column(2, 'In Review', Status.InReview),
-      new Column(3, 'Done', Status.Done),
+      new Column('To-Do', Status.ToDo, [
+         new Ticket('new To-Do', 'Description'),
+         new Ticket('new To-Do', 'Description'),
+         new Ticket('new To-Do', 'Description'),
+      ]),
+      new Column('In Progress', Status.InProgress, [
+         new Ticket('new In-Progress', 'Description'),
+      ]),
+      new Column('In Review', Status.InReview),
+      new Column('Done', Status.Done),
    ];
 
    constructor() {
@@ -32,20 +38,56 @@ class Board {
 }
 
 enum Status {
-   ToDo = 'ToDo',
-   InProgress = 'InProgress',
-   InReview = 'InReview',
-   Done = 'Done',
+   ToDo = 'to-do',
+   InProgress = 'in-progress',
+   InReview = 'in-review',
+   Done = 'done',
+}
+
+class Ticket {
+   private ticket: HTMLDivElement;
+   public status: Status = Status.ToDo;
+   private static nextId: number = 1;
+   private id: number;
+
+   constructor(title: string, description?: string) {
+      this.id = Ticket.nextId++;
+
+      this.ticket = document.createElement('div');
+      this.ticket.classList = `ticket ${this.status.toString()}-ticket`;
+      this.ticket.innerHTML = `
+      <div class="ticket-title">${title}</div>
+      ${description ? `<div class="ticket-description">${description}</div>` : ''}
+      `;
+   }
+
+   getElement() {
+      return this.ticket;
+   }
 }
 
 class Column {
    private colElement: HTMLDivElement;
+   private static nextId: number = 1;
+   private id: number;
 
-   constructor(id: number, title: string, status: Status) {
+   constructor(title: string, status: Status, tickets?: Ticket[]) {
+      this.id = Column.nextId++;
+
       this.colElement = document.createElement('div');
-      this.colElement.id = `col-${id}`;
+      this.colElement.id = `col-${this.id}`;
       this.colElement.className = `column ${status.toString()}`;
-      this.colElement.innerHTML = `<h2>${title}</h2>`;
+      this.colElement.innerHTML = `<div class="col-title">${title}</div>`;
+
+      if (tickets) {
+         this.colElement.innerHTML += `<div class="tickets"></div>`;
+
+         tickets.forEach((ticket) => {
+            this.colElement
+               .querySelector('.tickets')
+               ?.append(ticket.getElement());
+         });
+      }
    }
 
    getElement() {
